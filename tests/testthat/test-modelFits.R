c8d98c4f2fec6a0ab6b9c0a1dc8bc365165226ad
Eric S. Kawaguchi <ekawaguc@usc.edu>
2024-05-14 11:37:35 -0700

    1st update to re-submit to CRAN

--- tests/testthat/test-modelFits.R
+++ tests/testthat/test-modelFits.R
@@ -32,30 +32,30 @@ test_that("Compare crr with fastCrr w/ tied data", {
   expect_equal(as.vector(fit.crr$coef), as.vector(fit.fast$coef), tolerance = 1E-4)
 })
 
-
-test_that("Compare crrp with fastCrrp ", {
-  set.seed(4291)
-  ftime <- rexp(200)
-  fstatus <- sample(0:2,200,replace=TRUE)
-  cov <- matrix(runif(600),nrow=200)
-
-  #LASSO
-  fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "LASSO", lambda = 0.01)
-  fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "LASSO", lambda = 0.01)
-  expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
-
-  #SCAD
-  fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "SCAD", lambda = 0.01)
-  fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "SCAD", lambda = 0.01)
-  expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
-
-
-  #MCP
-  fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "MCP", lambda = 0.01)
-  fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "MCP", lambda = 0.01)
-  expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
-
-})
+# 02/27/24: Removed crrp check since crrp is not available on CRAN as of 02/27/24
+# test_that("Compare crrp with fastCrrp ", {
+#   set.seed(4291)
+#   ftime <- rexp(200)
+#   fstatus <- sample(0:2,200,replace=TRUE)
+#   cov <- matrix(runif(600),nrow=200)
+#
+#   #LASSO
+#   fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "LASSO", lambda = 0.01)
+#   fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "LASSO", lambda = 0.01)
+#   expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
+#
+#   #SCAD
+#   fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "SCAD", lambda = 0.01)
+#   fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "SCAD", lambda = 0.01)
+#   expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
+#
+#
+#   #MCP
+#   fit.crrp    <- crrp(ftime, fstatus, cov, penalty = "MCP", lambda = 0.01)
+#   fit.fast   <- fastCrrp(Crisk(ftime, fstatus) ~ cov, penalty = "MCP", lambda = 0.01)
+#   expect_equal(as.vector(fit.crrp$beta), as.vector(fit.fast$coef), tolerance = 1E-4)
+#
+# })
 
 test_that("Compare crr with fastCrr (breslow jumps)", {
   set.seed(4291)
